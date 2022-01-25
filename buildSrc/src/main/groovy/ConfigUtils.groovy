c82769ca81afa450157394628583c670e390d5eb
chentaikang <chentaikang_v@didichuxing.com>
2022-01-25 17:39:57 +0800

    新增lib，主要提供base及common两个包

--- buildSrc/src/main/groovy/ConfigUtils.groovy
+++ buildSrc/src/main/groovy/ConfigUtils.groovy
@@ -1,6 +1,39 @@
+import org.gradle.BuildListener
+import org.gradle.BuildResult
+import org.gradle.api.initialization.Settings
+import org.gradle.api.invocation.Gradle
+
 class ConfigUtils {
     static init(gradle){
         GLog.l('ConfigUtils init start...')
         TaskDurationUtils.init(gradle)
+    }
+    static addBuildListener(Gradle g) {
+        g.addBuildListener(new BuildListener() {
+//            @Override
+//            void buildStarted(Gradle gradle) {
+//                GLog.d("buildStarted")
+//            }
+
+            @Override
+            void settingsEvaluated(Settings settings) {
+                GLog.d("settingsEvaluated")
+            }
+
+            @Override
+            void projectsLoaded(Gradle gradle) {
+                GLog.d("projectsLoaded")
+            }
+
+            @Override
+            void projectsEvaluated(Gradle gradle) {
+                GLog.d("projectsEvaluated")
+            }
+
+            @Override
+            void buildFinished(BuildResult buildResult) {
+                GLog.d("buildFinished")
+            }
+        })
     }
 }
