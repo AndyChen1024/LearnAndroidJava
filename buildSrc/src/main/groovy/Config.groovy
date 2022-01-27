6244eed86a7d3a7fbdebd79c392b04e75185983a
chentaikang <chentaikang_v@didichuxing.com>
2022-01-27 14:26:08 +0800

    当前可编译版本

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -9,49 +9,48 @@ class Config {
     static versionCode = 1
     static versionName = '1.0.0'
 
-    static kotlin_version = '1.3.10'
+    static gradlePluginVersion = '7.0.4'
+
+    static kotlin_version = '1.3.50'
     static support_version = '27.1.1'
     static leakcanary_version = '1.6.3'
 
     static appcompat_version = "1.4.0"
-    static material_version = "1.4.0"
-    static constraintlayout_version = "2.1.2"
+    static material_version = "1.5.0"
+    static constraintlayout_version = "2.1.3"
 
-    static junit = "4.+"
+    static junit = "4.13.2"
     static android_junit = "1.1.3"
     static android_espresso = "3.4.0"
 
     static depConfig = [
-            plugin       : [
-                    gradle: "com.android.tools.build:gradle:7.0.4",
-                    kotlin: "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version",
-            ],
-            support      : [
-                    appcompat_v7    : "com.android.support:appcompat-v7:$support_version",
-                    design          : "com.android.support:design:$support_version",
-                    multidex        : "com.android.support:multidex:1.0.2",
-                    constraintlayout: "com.android.support.constraint:constraint-layout:1.1.3",
-            ],
-            androidx     : [
-                    appcompat       : "androidx.appcompat:appcompat:$appcompat_version",
-                    material        : "com.google.android.material:material:$material_version",
-                    constraintlayout: "androidx.constraintlayout:constraintlayout:$constraintlayout_version",
-            ],
-            junit        : "junit:junit:$junit",
-            test         : [
-                    android_junit   : "androidx.test.ext:junit:$android_junit",
-                    android_espresso: "androidx.test.espresso:espresso-core:$android_espresso",
-            ],
+            lib_base                   : new DepConfig(true, true, "lib:base"),
+            lib_common                 : new DepConfig(true, true, "lib:common"),
 
-            kotlin       : "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version",
-            utilcode     : "com.blankj:utilcode:1.25.0",
-            free_proguard: "com.blankj:free-proguard:1.0.1",
-            swipe_panel  : "com.blankj:swipe-panel:1.1",
+            plugin_gradle              : new DepConfig(pluginPath: "com.android.tools.build:gradle:$gradlePluginVersion"),
+            plugin_kotlin              : new DepConfig(pluginPath: "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"),
 
-            leakcanary   : [
-                    android         : "com.squareup.leakcanary:leakcanary-android:$leakcanary_version",
-                    android_no_op   : "com.squareup.leakcanary:leakcanary-android-no-op:$leakcanary_version",
-                    support_fragment: "com.squareup.leakcanary:leakcanary-support-fragment:$leakcanary_version",
-            ],
+            support_appcompat_v7       : new DepConfig("com.android.support:appcompat-v7:$support_version"),
+            support_design             : new DepConfig("com.android.support:design:$support_version"),
+            support_multidex           : new DepConfig("com.android.support:multidex:1.0.2"),
+            support_constraintlayout   : new DepConfig("com.android.support.constraint:constraint-layout:1.1.3"),
+
+            androidx_appcompat         : new DepConfig("androidx.appcompat:appcompat:$appcompat_version"),
+            androidx_material          : new DepConfig("com.google.android.material:material:$material_version"),
+            androidx_constraintlayout  : new DepConfig("androidx.constraintlayout:constraintlayout:$constraintlayout_version"),
+
+//            junit                      : new DepConfig("junit:junit:$junit"),
+//            test_android_junit         : new DepConfig("androidx.test.ext:junit:$android_junit"),
+//            test_android_espresso      : new DepConfig("androidx.test.espresso:espresso-core:$android_espresso"),
+
+            kotlin                     : new DepConfig("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"),
+//            utilcode                   : new DepConfig("com.blankj:utilcode:1.25.0"),
+//            free_proguard              : new DepConfig("com.blankj:free-proguard:1.0.1"),
+//            swipe_panel                : new DepConfig("com.blankj:swipe-panel:1.1"),
+
+//            leakcanary_android         : new DepConfig("com.squareup.leakcanary:leakcanary-android:$leakcanary_version"),
+//            leakcanary_android_no_op   : new DepConfig("com.squareup.leakcanary:leakcanary-android-no-op:$leakcanary_version"),
+//            leakcanary_support_fragment: new DepConfig("com.squareup.leakcanary:leakcanary-support-fragment:$leakcanary_version"),
+
     ]
 }
--- buildSrc/src/main/groovy/ConfigUtils.groovy
+++ buildSrc/src/main/groovy/ConfigUtils.groovy
@@ -1,11 +1,16 @@
 import org.gradle.BuildListener
 import org.gradle.BuildResult
+import org.gradle.api.Project
+import org.gradle.api.ProjectEvaluationListener
+import org.gradle.api.ProjectState
 import org.gradle.api.initialization.Settings
 import org.gradle.api.invocation.Gradle
 
 class ConfigUtils {
     static init(gradle){
         GLog.l('ConfigUtils init start...')
+        generateDep(gradle)
+        addCommonGradle(gradle)
         TaskDurationUtils.init(gradle)
     }
     static addBuildListener(Gradle g) {
@@ -35,5 +40,103 @@ class ConfigUtils {
                 GLog.d("buildFinished")
             }
         })
+    }
+
+    private static void generateDep(Gradle gradle) {
+        def configs = [:]
+        for (Map.Entry<String, DepConfig> entry : Config.depConfig.entrySet()) {
+            def (name, config) = [entry.key, entry.value]
+            if (name.startsWith("plugin_")) {
+                config.dep = config.pluginPath
+            } else {
+                if (config.useLocal) {
+                    config.dep = gradle.rootProject.findProject(config.localPath)
+                } else {
+                    config.dep = config.remotePath
+                }
+            }
+            configs.put(name, config)
+        }
+        GLog.l("generateDep = ${GLog.object2String(configs)}")
+    }
+    private static addCommonGradle(Gradle gradle) {
+        gradle.addProjectEvaluationListener(new ProjectEvaluationListener() {
+            @Override
+            void beforeEvaluate(Project project) {
+                // 在 project 的 build.gradle 前 do sth.
+                if (project.subprojects.isEmpty()) {
+                    if (project.path.contains(":plugin:")) {
+                        return
+                    }
+//                    if (project.name.endsWith("_app")) {
+                    if (project.name.contains("app")) {
+                        GLog.l(project.toString() + " applies buildApp.gradle")
+                        project.apply {
+                            from "${project.rootDir.path}/buildApp.gradle"
+                        }
+                    } else {
+                        GLog.l(project.toString() + " applies buildLib.gradle")
+                        project.apply {
+                            from "${project.rootDir.path}/buildLib.gradle"
+                        }
+                    }
+                }
+            }
+
+            @Override
+            void afterEvaluate(Project project, ProjectState state) {
+                // 在 project 的 build.gradle 末 do sth.
+            }
+        })
+    }
+    static getApplyPlugins() {
+        def plugins = [:]
+        for (Map.Entry<String, DepConfig> entry : Config.depConfig.entrySet()) {
+            if (entry.value.isApply && entry.key.startsWith("plugin_")) {
+                plugins.put(entry.key, entry.value)
+            }
+        }
+        GLog.d("getApplyPlugins = ${GLog.object2String(plugins)}")
+        return plugins
+    }
+
+    static getAppDeps() {
+        def pkgs = [:]
+        for (Map.Entry<String, DepConfig> entry : Config.depConfig.entrySet()) {
+            if (entry.value.isApply && !entry.key.startsWith("lib_")&&!entry.key.startsWith("plugin_")) {
+//            if (entry.value.isApply) {
+                pkgs.put(entry.key, entry.value)
+            }
+        }
+        GLog.d("getAppDeps = ${GLog.object2String(pkgs)}")
+        return pkgs
+    }
+
+    static getApplyPkgs() {
+        def pkgs = [:]
+        for (Map.Entry<String, DepConfig> entry : Config.depConfig.entrySet()) {
+            if (entry.value.isApply && entry.key.endsWith("_pkg")) {
+//            if (entry.value.isApply) {
+                pkgs.put(entry.key, entry.value)
+            }
+        }
+        GLog.d("getApplyPkgs = ${GLog.object2String(pkgs)}")
+        return pkgs
+    }
+
+    static getApplyExports() {
+        def exports = [:]
+//        for (Map.Entry<String, DepConfig> entry : Config.depConfig.entrySet()) {
+//            if (entry.value.isApply && entry.key.endsWith("_export")) {
+//                exports.put(entry.key, entry.value)
+//            }
+//        }
+//        for (Map.Entry<String, DepConfig> entry : Config.depConfig.entrySet()) {
+//            if (entry.value.isApply && entry.key.contains("base")) {
+//                exports.put(entry.key, entry.value)
+//            }
+//        }
+        GLog.d("getApplyExports = ${GLog.object2String(exports)}")
+        return exports
     }
 }
